d89e26e2676bddc31cf67f9f6dc8dfe4e57406e0
Tan Haoxiang <3123867599@qq.com>
2024-07-06 14:58:42 +0800

    Modify node_append() def after merging

--- include/db/indexing.h
+++ include/db/indexing.h
@@ -65,7 +65,7 @@ namespace db {
 
         // 在给定节点后连接一个新节点，并返回该新节点
         // ATTENTION: 直接返回节点不确定是否会出现bug
-        Node node_append(Node *node, bool is_leaf);
+        Node node_append(Node *node);
 
         // 获取根节点id
         std::pair<bool, unsigned int> get_root();
